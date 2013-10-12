606324bbcd902ffb5273baf0e1adf94c53cf4c1c
thomaschen <99703040@nccu.edu.tw>
2013-10-12 13:49:33 +0800

    render items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Nccu2hand::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  root :to => 'high_voltage/pages#show', :id => 'welcome'
+  # root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => 'items#index'
 
   resources :items
     match '/new' => 'items#new'
