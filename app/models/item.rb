6a3cc078aae5c8bfbcf17e8040538491b98ab9c1
Bernie Chiu <bernie_chiu@hotmail.com>
2013-10-12 11:06:56 +0800

    Model relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :description, :price, :status, :title
+  belongs_to :user
+
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
+  has_many :items
+
   def admin?
     Setting.admin_emails.include?(email)
   end
