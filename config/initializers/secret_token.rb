5ec4fc7b0098339c18cf08564c05ba60564983af
Bernie Chiu <bernie_chiu@hotmail.com>
2013-10-12 09:26:58 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nccu2hand::Application.config.secret_token = '5eef9de652a0bc64fe2d4706a4c691ceaf8c208b39ee8ca9320cf0f2ba721f4884398ca034100bd1a59a73311699079a153899031c2e835d64dc76edc60037b8'
